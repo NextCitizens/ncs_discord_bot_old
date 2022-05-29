82bca5e7f04dc1203eb59e2c1b1c81b657303fc5
Pablo Zapata <pablo.zapata.dev@gmail.com>
2022-05-29 19:51:32 +0200

    :wrench: Add a Constant class

---
+++ src/main/java/fr/pablozapata/ncsbot/Constant.java
@@ -0,0 +1,9 @@
+package fr.pablozapata.ncsbot;
+
+public class Constant {
+    public static final Long NCS_GUILD_ID = 979505357091135498L;
+
+    public static final class Channels {
+        public static final Long FLUX_CHANNEL_ID = 980525401644367903L;
+    }
+}
