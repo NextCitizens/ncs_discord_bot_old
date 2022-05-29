08c58be4bbe7df98812f272644e6af55ecda537d
Pablo Zapata <pablo.zapata.dev@gmail.com>
2022-05-29 19:50:44 +0200

    :sparkles: Add Anti-Invite on message

---
+++ src/main/java/fr/pablozapata/ncsbot/events/listeners/MessageReceived.java
@@ -0,0 +1,34 @@
+package fr.pablozapata.ncsbot.events.listeners;
+
+import fr.pablozapata.ncsbot.Main;
+import fr.pablozapata.ncsbot.utils.MessageUtils;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import org.jetbrains.annotations.NotNull;
+
+public class MessageReceived {
+    public MessageReceived(@NotNull MessageReceivedEvent event) {
+        final var author = event.getAuthor();
+        final var message = event.getMessage();
+        final var channelType = event.getChannelType();
+        final var channel = event.getChannel();
+        final var member = event.getMember();
+        final var guild = event.getGuild();
+
+        if (!(channelType.isMessage() && channelType.isGuild())) {
+            return;
+        }
+
+        if (!guild.equals(Main.getNcsGuild())) {
+            return;
+        }
+
+        // Anti invitations system
+        final var hasInvites = MessageUtils.inviteCheck(event);
+
+        if (hasInvites) {
+            message.delete().submit();
+            channel.sendMessage(String.format("<:deny:980521301443760228> **»** %s les invitations Discord sont interdites !", member.getAsMention())).submit();
+            return;
+        }
+    }
+}
